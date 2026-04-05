fd6335435fe45f22fbea1f4870f149da2c0af500
Developer Kush 👨🏼‍💻 || Full stack web developer 💪🏼 <kushpandit68775@gmail.com>
2026-04-05 20:01:23 +0530

    routes change testing

--- src/App.jsx
+++ src/App.jsx
@@ -96,7 +96,7 @@ export default function App() {
       <Layout>
         <Routes>
           <Route path="/toolify" element={<Home />} />
-          <Route path="/tools/json-to-csv" element={<JsonToCsv />} />
+          <Route path="/toolify/tools/json-to-csv" element={<JsonToCsv />} />
           <Route path="/tools/csv-to-json" element={<CsvToJson />} />
           <Route path="/tools/excel-to-csv" element={<ExcelToCsv />} />
           <Route path="/tools/csv-to-excel" element={<CsvToExcel />} />
